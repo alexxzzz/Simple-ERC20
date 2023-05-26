24583d8d687d446d97f68b78b819de0feffa718e
alexxzzz <alejandrorial01@gmail.com>
2023-05-26 10:21:43 +0200

    upload code

---
+++ contracts/customERC20.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "contracts/ERC-20.sol";
+
+contract customERC20 is ERC20 {
+
+    // Constructor del Smart Contract
+    constructor() ERC20("ZoeCoin", "ZOE"){}
+
+    // Creation of new Tokens
+    function createTokens() public {
+        address minter = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+        require(msg.sender == minter, "ERC20: only the creator can mint tokens");
+        _mint(msg.sender, 100);
+    }
+}
